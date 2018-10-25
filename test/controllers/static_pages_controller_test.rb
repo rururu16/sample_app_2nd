a7a72b44d0a5c17b62eba9b8e62c94cc79a48206
rururu16 <ynk618172@gmail.com>
2018-10-25 21:05:33 +0900

    Mod layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,32 +6,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "rootページを表示" do
-    get root_url
+  test "rootページ(home)を表示" do
+    get root_path
     assert_response :success
     assert_select "title","#{@base_title}"
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title","#{@base_title}"
-  end
-
-  test "should get help" do
-    get static_pages_help_url
+  test "helpページを表示" do
+    get help_path
     assert_response :success
     assert_select "title","Help | #{@base_title}"
   end
 
   test "aboutページを表示" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title","About | #{@base_title}"
   end
 
   test "contactページを表示" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title","Contact | #{@base_title}"
   end
