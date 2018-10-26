6adca5873fb6bcb72afef66c086dd06fd31d760b
rururu16 <ynk618172@gmail.com>
2018-10-26 13:21:57 +0900

    add email format validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,5 +19,30 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "nameの文字数超過" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
 
+  test "emailの文字数超過" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "emailのフォーマット：有効" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} は有効"
+    end
+  end
+  test "emailのフォーマット：無効" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} は無効"
+    end
+  end
 end
